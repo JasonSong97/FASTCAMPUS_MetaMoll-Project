12c98bd0b6a5e7373b36ce180ccc279981182e4c
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-04-10 20:04:05 +0900

    FEAT: add model

---
+++ src/main/java/shop/mtcoding/metamall/model/product/Product.java
@@ -0,0 +1,76 @@
+package shop.mtcoding.metamall.model.product;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import shop.mtcoding.metamall.model.user.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Setter // DTO 만들면 삭제해야됨
+@Getter
+@Table(name = "product_tb")
+@Entity
+public class Product { // N
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne // N:1
+    private User seller; // seller_id
+
+    @Column(nullable = false, length = 50)
+    private String name; // 상품 이름
+
+    @Column(nullable = false)
+    private Integer price; // 상품 가격
+
+    @Column(nullable = false)
+    private Integer qty; // 상품 재고
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    // 상품 변경 (판매자 입장)
+    public void update(String name, int price, int qty) {
+        this.name = name;
+        this.price = price;
+        this.qty = qty;
+    }
+
+    // 주문시 재고 변경 (구매자 입장)
+    public void updateQty(Integer orderCount) {
+        if (this.qty < orderCount) {
+            // checkpoint 주문수량이 지고 수량을 초과했습니다.
+        }
+        this.qty = this.qty - orderCount;
+    }
+
+    // 주문 취소 재고 변경 (구매자, 판매자)
+    public void rollbackQty(Integer orderCount) {
+        this.qty = this.qty + orderCount;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @Builder
+    public Product(Long id, User seller, String name, Integer price, Integer qty, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.seller = seller;
+        this.name = name;
+        this.price = price;
+        this.qty = qty;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
---
+++ src/main/java/shop/mtcoding/metamall/model/user/User.java
@@ -0,0 +1,77 @@
+package shop.mtcoding.metamall.model.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Setter // DTO 만들면 삭제해야됨
+@Getter
+@Table(name = "user_tb")
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 20) // unique 제약조건
+    private String username;
+
+    @JsonIgnore // 노출되지 않기 위해서
+    @Column(nullable = false, length = 60) // 60Byte: 단방향 암호화(60자 이상)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @Column(nullable = false, length = 10)
+    private String role; // USER(고객), SELLER(판매자), ADMIN(관리자)
+
+    @Column(nullable = false, length = 10)
+    private Boolean status; // true: 활성, false: 비활성 계정
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    // 권한 변경 (관리자)
+    public void updateRole(String role) { // 의미있는 setter로 role 바꾸기
+        if (this.role.equals(role)) {
+            // checkpoint 동일한 권한으로 변경할 수 없습니다.
+        }
+        this.role = role;
+    }
+
+    // 회원 탈퇴
+    public void delete(){
+        this.status = false;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @Builder
+
+    public User(Long id, String username, String password, String email, String role, Boolean status, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
